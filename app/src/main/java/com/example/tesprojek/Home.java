393d2fad1a20da185ecb721591e65a67fa825aa3
IkbalFiqri <fourceikbal09@gmail.com>
2021-06-05 01:52:06 +0700

    Project Mobile

---
+++ app/src/main/java/com/example/tesprojek/Home.java
@@ -0,0 +1,19 @@
+package com.example.tesprojek;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class Home extends AppCompatActivity{
+    Button bTambah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+    };
+}
